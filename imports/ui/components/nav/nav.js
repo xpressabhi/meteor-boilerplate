e4d322e295948be197a1d5960cd56dfd5a998d3e
abhishek <akmnitt@gmail.com>
2018-01-11 12:02:18 +0530

    gravator

--- imports/ui/components/nav/nav.js
+++ imports/ui/components/nav/nav.js
@@ -9,6 +9,29 @@ Template.nav.helpers({
   useremail(){
     const user = Meteor.user();
     return user && user.emails && user.emails[0].address;
+  },
+  gravatorUrl: function() {
+    const user = Meteor.user();
+    const email = user && user.emails && user.emails[0].address;
+    let url;
+    if (email) {
+      var options = {
+        secure: true
+      };
+
+      //  var md5Hash = Gravatar.hash(email);
+      // 5658ffccee7f0ebfda2b226238b1eb6e
+
+      url = Gravatar.imageUrl(email, options);
+      // https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e
+    //  console.log(url);
+
+      //  var url2 = Gravatar.imageUrl(md5Hash, options);
+      // https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e
+      //  console.log(url2);
+    }
+    return url;
+
   }
 });
 
