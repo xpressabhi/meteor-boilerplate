235e04cac31d18719b459eaa4678429aa8ccea36
abhishek <akmnitt@gmail.com>
2018-05-08 18:56:44 +0530

    account temp

--- imports/startup/both/index.js
+++ imports/startup/both/index.js
@@ -1,2 +1,30 @@
 // Import modules used by both client and server through a single index entry point
 // e.g. useraccounts configuration file.
+
+AccountsTemplates.addField({
+  _id: 'type',
+  type: 'hidden'
+});
+
+AccountsTemplates.configure({
+  //defaultTemplate: 'Auth_page',
+  defaultLayout: 'App_body',
+  defaultContentRegion: 'main',
+  defaultLayoutRegions: {nav: 'nav'}
+});
+
+AccountsTemplates.configureRoute('signIn', {
+  name: 'signin',
+  path: '/signin',
+  redirect: '/'
+});
+AccountsTemplates.configureRoute('signUp', {
+  name: 'join',
+  path: '/join',
+  redirect: '/'
+});
+// AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('resetPwd', {
+  name: 'resetPwd',
+  path: '/reset-password'
+});
