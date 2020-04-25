ba1e3dcbb3ec492338944bc22da16d70ad3c8bcd
abhishek <akmnitt@gmail.com>
2020-04-25 09:11:36 +0530

    update 1.10.2

--- imports/ui/components/users/users.js
+++ imports/ui/components/users/users.js
@@ -1,26 +1,18 @@
-import {Meteor} from 'meteor/meteor';
+import {
+  Meteor
+} from 'meteor/meteor';
 import './users.html';
 import './../../helpers/helpers.js';
 
 Template.users.onCreated(function() {
-  // counter starts at 0
-  const self = this;
-  self.searchQuery = new ReactiveVar('');
-  self.searching = new ReactiveVar(false);
-  self.autorun(() => {
-    self.subscribe("users.search", self.searchQuery.get(), () => {
-      setTimeout(() => {
-        this.searching.set(false);
-      }, 300);
-    });
+  this.searchQuery = new ReactiveVar('');
+  this.limit = new ReactiveVar(5);
+  this.autorun(() => {
+    this.subscribe("users.search", this.searchQuery.get(), this.limit.get());
   });
-
 });
 
 Template.users.helpers({
-  panelClass() {
-    return 'panel-primary';
-  },
   users() {
     return Meteor.users.find({}, {
       sort: {
@@ -28,35 +20,19 @@ Template.users.helpers({
       }
     });
   },
-  useremail() {
-    return this.emails[0].address;
-  },
-  isAdmin() {
-    return Roles.userIsInRole(this._id, 'admin');
-  },
-  searching(){
-    return Template.instance().searching.get();
-  }
 });
 
 Template.users.events({
-  'click .deleteUser' (event, templateInstance) {
-    Meteor.call('users.remove', this._id, (e, r) => {
-      if (e)
-        console.log(e);
-      if (r)
-        console.log(r);
-      }
-    );
+  'click .deleteUser'(e, t) {
+    Meteor.call('users.remove', this._id, (eee, res) => {
+      err && console.log(err);
+      res && console.log(res);
+    });
   },
-  'keyup input[name="users"]' (event, templateInstance) {
-    const txt = ev.target.value.trim();
-    t.searchQuery.set(txt);
-    t.searching.set(true);
+  'keyup input[name="users"]'(e, t) {
+    t.searchQuery.set(e.target.value.trim());
   },
-  'click .toggleRole'(event, templateInstance){
-    const role = event.target.innerText.toLowerCase();
-    Meteor.call('user.toggleRole', this._id, role);
+  'click .toggleRole'(e, t) {
+    Meteor.call('user.toggleRole', this._id, e.target.innerText.toLowerCase(), () => {});
   }
-
 });
